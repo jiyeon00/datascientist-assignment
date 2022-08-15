0b8a0aab1e3faed295217ea815ba4c838bb26732
jiyeon00 <99636770+jiyeon00@users.noreply.github.com>
2022-08-15 18:24:10 +0900

    전지연 answer2.sql

--- answer2.sql
+++ answer2.sql
@@ -1,1 +1,15 @@
--- ANSWER 2
+CREATE TABLE answer2(author_id TEXT, year INTEGER ,publication_count INTEGER, citation_count INTEGER);
+
+INSERT INTO answer2(author_id, year, publication_count, citation_count)
+SELECT 
+   author_id, cast(yyyy as integer) yyyy, count(paper_id) , sum(citation_count)
+FROM
+   (
+   SELECT author_id, substr(published_at,1,4) yyyy, paper_author.paper_id, citation_count
+   FROM paper_author
+   LEFT OUTER JOIN citation_count_table
+   ON paper_author.paper_id = citation_count_table.paper_id
+   )
+GROUP BY author_id , yyyy ;
+
+SELECT * FROM answer2;
