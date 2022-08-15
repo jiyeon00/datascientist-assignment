5d92e63f00f03b93f4547722c6f6f837bb011f1a
jiyeon00 <99636770+jiyeon00@users.noreply.github.com>
2022-08-15 18:25:14 +0900

    전지연 answer4.sql

--- answer4.sql
+++ answer4.sql
@@ -1,1 +1,21 @@
 -- ANSWER 4
+CREATE TABLE answer4 (author_id TEXT, hindex INTEGER)
+
+INSERT INTO answer4(author_id, hindex)
+SELECT author_id, min(ValRank) hindex
+FROM
+   (SELECT 
+      *, 
+      RANK () OVER ( 
+         ORDER BY citation_count DESC
+      ) ValRank
+   FROM
+      (
+      SELECT author_id, substr(published_at,1,7) yyyymm, paper_author.paper_id, citation_count
+      FROM paper_author
+      LEFT OUTER JOIN citation_count_table
+      ON paper_author.paper_id = citation_count_table.paper_id
+      )
+   WHERE author_id = 'a31')
+WHERE citation_count <= ValRank
+;
